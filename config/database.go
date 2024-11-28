621c7b7bff412c5ebcc384caa516484e11b5e265
Triatmono <162248336+Triatmono@users.noreply.github.com>
2024-11-29 00:16:08 +0700

    Commit
    Feat : Added CRUD for 2 tables. Front page still need to be revise.

--- config/database.go
+++ config/database.go
@@ -10,7 +10,8 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	db, err := sql.Open("mysql", "root: @/go_products")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/go_products?charset=utf8mb4&parseTime=True&loc=Local")
+	// db, err := sql.Open("mysql", "root: @/go_products")
 	if err != nil {
 		panic(err)
 	}
--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -1,9 +1,12 @@
 package categorycontroller
 
 import (
+	"go-web-native/entities"
 	"go-web-native/models/categorymodel"
-	"text/template"
 	"net/http"
+	"strconv"
+	"text/template"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -13,7 +16,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	temp, err := template.ParseFiles("views/category/index.html")
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
@@ -21,13 +24,83 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
-	
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/category/create.html")
+		if err != nil {
+			panic(err)
+		}
+
+		temp.Execute(w, nil)
+	}
+	if r.Method == "POST" {
+		var category entities.Category
+
+		category.Name = r.FormValue("name")
+		category.CreatedAt = time.Now()
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Create(category); !ok {
+			temp, _ := template.ParseFiles("views/category/create.html")
+			temp.Execute(w, nil)
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
-	
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/category/edit.html")
+		if err != nil {
+			panic(err)
+		}
+
+		idString := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			panic(err)
+		}
+
+		category := categorymodel.Detail(id)
+		data := map[string]any{
+			"category": category,
+		}
+
+		temp.Execute(w, data)
+	}
+
+	if r.Method == "POST" {
+		var category entities.Category
+
+		idString := r.FormValue("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category.Name = r.FormValue("name")
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Update(id, category); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-	
+	idString := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if err := categorymodel.Delete(id); err != nil {
+			panic(err)
+		}
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
 }
