7ad458bfd9aa5eaae4a5f0076ca9b45e84e93bb6
Haiyan Hazrin Bin Haimidon <hazrinhaiyan987@gmail.com>
2024-12-08 10:40:52 +0700

    Adding Display features of updated stock

--- controllers/homecontroller/homecontroller.go
+++ controllers/homecontroller/homecontroller.go
@@ -3,13 +3,20 @@ package homecontroller
 import (
 	"net/http"
 	"text/template"
+	"go-web-native/models/productmodel"
 )
 
 func Welcome(w http.ResponseWriter, r *http.Request) {
+
+	products := productmodel.GetAll()
+	data := map[string]any{
+		"products": products,
+	}
+
 	temp, err := template.ParseFiles("views/home/index.html")
 	if err != nil {
 		panic(err)
 	}
 
-	temp.Execute(w, nil)
+	temp.Execute(w, data)
 }
